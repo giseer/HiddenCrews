bd250bbb3ac9489bce93edc60bc3a2a99b38f9d7
Sergi Rebollo <sergireb2010@hotmail.com>
2024-03-04 12:37:59 +0100

    ReCreat Animation Rigging System
    He rehecho el sistema de animation Rigging, he borrado las animaciones de disparo así las creamos nosotros desde cero, solo dejo las de movimiento.  Hecho movimiento pistola, bate, rifle de asalto

--- Assets/Character/Player/Scripts/PlayerAnimationsHandler.cs
+++ Assets/Character/Player/Scripts/PlayerAnimationsHandler.cs
@@ -2,16 +2,5 @@ using UnityEngine;
 
 public class PlayerAnimationsHandler : MonoBehaviour
 {
-    [Header("Components")] 
-    [SerializeField] private Animator animator;
-    
-    public void ActiveAimingAnimations()
-    {   
-        animator.SetBool("Aiming", true);
-    }
-    
-    public void DesactiveAimingAnimations()
-    {   
-        animator.SetBool("Aiming", false);
-    }
+
 }
