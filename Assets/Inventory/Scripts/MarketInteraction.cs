47cb335a98fa9bfa5409bbc6fe8c86368520d8e2
pacana610 <145553562+pacana610@users.noreply.github.com>
2024-03-06 17:20:57 +0100

    Inventory and description COMPLETE

--- Assets/Inventory/Scripts/MarketInteraction.cs
+++ Assets/Inventory/Scripts/MarketInteraction.cs
@@ -7,49 +7,85 @@ using UnityEngine.UI;
 public class MarketInteraction : MonoBehaviour
 {
     public GameObject marketCanvas;
-    public List<Button> armaMarket;
+    public List<Canvas> listaDeCanvas;
+    public List<Button> armaMarket1;
     public RawImage otroCanvas;
     public GameObject player;
 
-    public List<Canvas> listaDeCanvasMensaje;
+    private Canvas canvasActivo;
 
-    private Button botonActivo;
+    void Start()
+    {
+        foreach (var canvas in listaDeCanvas)
+        {
+            Button button = canvas.GetComponentInChildren<Button>();
+            if (button != null)
+            {
+                button.onClick.AddListener(() => OnClick(canvas));
+            }
+        }
+
+        if (listaDeCanvas.Count > 0)
+        {
+            canvasActivo = listaDeCanvas[0];
+        }
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
             ToggleMarketVisibility();
-
         }
     }
 
-    public void OnClick(Button armaMarket2)
+    public void OnClick(Canvas nuevoCanvas)
     {
-        if (armaMarket.Contains(armaMarket2))
+        DesactivarTodosLosCanvas();
+
+        ActivarCanvasMensaje(nuevoCanvas);
+
+        canvasActivo = nuevoCanvas;
+    }
+
+    public void Mensaje()
+    {
+        Debug.Log("HOLA"); 
+    }
+
+    public void OnClickButton(Button armaMarket2)
+    {
+        if (armaMarket1.Contains(armaMarket2))
         {
-            armaMarket2.gameObject.SetActive(false);
+            armaMarket2.GetComponent<Button>().enabled = false;
+
+            armaMarket2.onClick = new Button.ButtonClickedEvent();
 
             armaMarket2.transform.SetParent(otroCanvas.transform, false);
             armaMarket2.gameObject.SetActive(true);
 
-            int index = armaMarket.IndexOf(armaMarket2);
+            int index = armaMarket1.IndexOf(armaMarket2);
 
-            if (index >= 0 && index < listaDeCanvasMensaje.Count)
+            if (index >= 0 && index < listaDeCanvas.Count)
             {
-                Canvas canvasMensaje = listaDeCanvasMensaje[index];
-
-                
+                Canvas canvasMensaje = listaDeCanvas[index];
 
                 ActivarCanvasMensaje(canvasMensaje);
             }
         }
     }
 
+    void DesactivarTodosLosCanvas()
+    {
+        foreach (var canvas in listaDeCanvas)
+        {
+            DesactivarCanvasMensaje(canvas);
+        }
+    }
+
     void ToggleMarketVisibility()
     {
         player.SetActive(marketCanvas.gameObject.activeSelf);
-
 
         marketCanvas.gameObject.SetActive(!marketCanvas.gameObject.activeSelf);
 
@@ -59,10 +95,8 @@ public class MarketInteraction : MonoBehaviour
 
         if (!marketCanvas.gameObject.activeSelf)
         {
-            DesactivarTodosLosCanvasMensaje();
+            DesactivarCanvasMensaje(canvasActivo);
         }
-
-
     }
 
     public void ActivarCanvasMensaje(Canvas canvas)
@@ -78,14 +112,6 @@ public class MarketInteraction : MonoBehaviour
         if (canvas != null)
         {
             canvas.gameObject.SetActive(false);
-        }
-    }
-
-    void DesactivarTodosLosCanvasMensaje()
-    {
-        foreach (Canvas canvasMensaje in listaDeCanvasMensaje)
-        {
-            DesactivarCanvasMensaje(canvasMensaje);
         }
     }
 }
