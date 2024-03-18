48905e56f269f6ba98600c59ca6c518e5f9c7087
Sergi Rebollo <sergireb2010@hotmail.com>
2024-03-18 14:30:36 +0100

    18032024

--- Assets/Missions/Scripts/MurderMission.cs
+++ Assets/Missions/Scripts/MurderMission.cs
@@ -1,24 +1,73 @@
+using System;
+using UnityEngine;
+
 public class MurderMission : Mission
 {
     private NPCMovement target;
+
+    [SerializeField] private float missionTime;
+
+    [HideInInspector] public static float currentTime;
+
+    private bool missionFinished;
+
+    [Header("Canvas Settings")] 
+    [SerializeField] private Transform failedCanvas;
+    [SerializeField] private Transform passedCanvas;
     
+    
+    private void Awake()
+    {
+        currentTime = missionTime;
+        missionFinished = false;
+    }
+
+    private void Update()
+    {
+        checkFinishConditions();
+    }
+
     protected override void checkFinishConditions()
     {
-        // Ganas si:
-        // La vida del target es 0
-        
-        // Pierdes si:
-        // Pasa el tiempo
-        // Si mueres
+        CheckEnemyCount();
+
+        CheckMissionTime();
+    }
+
+    private void CheckEnemyCount()
+    {
+        if (EnemyHealther.enemyCount <=0)
+        {
+            OnPassed();
+        }
+    }
+    
+    private void CheckMissionTime()
+    {
+        if (!missionFinished)
+        {
+            if (currentTime <= 0)
+            {
+                OnFailed();
+                currentTime = 0;
+                missionFinished = true;
+            }
+            else
+            {
+                currentTime -= Time.deltaTime;       
+            }    
+        }
     }
 
     protected override void OnPassed()
     {
-        throw new System.NotImplementedException();
+        passedCanvas.gameObject.SetActive(true);
+        Time.timeScale = 0.5f;
     }
 
     protected override void OnFailed()
     {
-        throw new System.NotImplementedException();
+        failedCanvas.gameObject.SetActive(true);
+        Time.timeScale = 0.5f;
     }
 }
