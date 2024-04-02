aefafdc7925145640e54f142fa428888529b95aa
pacana610 <145553562+pacana610@users.noreply.github.com>
2024-04-02 17:44:49 +0200

    ProductMission

---
+++ Assets/Missions/Scripts/Prueba.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Prueba : MonoBehaviour
+{
+    public float radio = 3f; // Radio del área de detección
+    public GameObject mensajeTexto; // Referencia al objeto Text en el Canvas
+    public GameObject cubo; // Referencia al cubo
+    private bool dentroDelArea = false; // Indicador de si el jugador está dentro del área
+    private bool cuboDesaparecido = false; // Indicador de si el cubo ha desaparecido
+
+    void Update()
+    {
+        // Se obtienen todos los colliders dentro del área esférica
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radio);
+
+        // Se verifica si el jugador está dentro del área
+        dentroDelArea = false;
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("Player"))
+            {
+                dentroDelArea = true;
+                break;
+            }
+        }
+
+        // Actualizar el estado del Canvas
+        if (dentroDelArea && !cuboDesaparecido)
+        {
+            mensajeTexto.SetActive(true); // Activar el Canvas cuando el jugador está dentro del área y el cubo no ha desaparecido
+        }
+        else
+        {
+            mensajeTexto.SetActive(false); // Desactivar el Canvas permanentemente cuando el cubo ha desaparecido
+        }
+
+        // Desactivar el cubo al presionar la tecla R
+        if (mensajeTexto.activeSelf && Input.GetKeyDown(KeyCode.R))
+        {
+            cubo.SetActive(false);
+            cuboDesaparecido = true; // Establecer el indicador de que el cubo ha desaparecido
+        }
+    }
+}
--- Assets/Missions/Scripts/RecollectMission.cs
+++ Assets/Missions/Scripts/RecollectMission.cs
@@ -4,25 +4,67 @@ public class RecollectMission : Mission
 {
     private Products product;
     private Transform location;
-    
+
+    [SerializeField] private float missionTime;
+
+    [HideInInspector] public static float currentTime;
+
+    private bool missionFinished;
+
+    [Header("Canvas Settings")]
+    [SerializeField] private Transform failedCanvas;
+    [SerializeField] private Transform passedCanvas;
+
+
+    private void Awake()
+    {
+        currentTime = missionTime;
+        missionFinished = false;
+    }
+
+    private void Update()
+    {
+        checkFinishConditions();
+    }
+
     protected override void checkFinishConditions()
     {
-        // Ganas si:
-        // Recollectas todos los productos
+        CheckEnemyCount();
+
+        CheckMissionTime();
+    }
+
+    private void CheckEnemyCount()
+    {
         
-        // Pierdes si:
-        // Pasa el tiempo
-        // Te pilla la policia
-        // Te mueres
+    }
+
+    private void CheckMissionTime()
+    {
+        if (!missionFinished)
+        {
+            if (currentTime <= 0)
+            {
+                OnFailed();
+                currentTime = 0;
+                missionFinished = true;
+            }
+            else
+            {
+                currentTime -= Time.deltaTime;
+            }
+        }
     }
 
     protected override void OnPassed()
     {
-        throw new System.NotImplementedException();
+        passedCanvas.gameObject.SetActive(true);
+        Time.timeScale = 0.5f;
     }
 
     protected override void OnFailed()
     {
-        throw new System.NotImplementedException();
+        failedCanvas.gameObject.SetActive(true);
+        Time.timeScale = 0.5f;
     }
 }
